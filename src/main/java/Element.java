2b45d78afc0645521bbaffcd3a5f48e9f8d6c99e
tasya <taisiya_struchaeva@mail.ru>
2022-03-07 19:54:46 +0300

    exercise_1

---
+++ src/main/java/Element.java
@@ -0,0 +1,24 @@
+public class Element<T> {
+    private T value;
+    private Element<T> next;
+
+    public Element(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public Element<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Element<T> next) {
+        this.next = next;
+    }
+}
